b10ab5359b9dec7c5e4c725998871b85b17cb0f4
lallafm1984 <lallafm1984@gmail.com>
2025-03-20 15:16:40 +0900

    03200316

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link"
 import { Menu } from "lucide-react"
 import { Button } from "@/components/ui/button"
-import MobileNav from "@/components/mobile-nav"
 
 export default function Header() {
   return (
